972553ad4df893880dda4519a3d00c5e0fd629af
Andrew <astrelcov@yahoo.com>
2020-11-24 22:41:22 +0200

    cicle

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
 "use strict";
 
-if (4==4) {
-    console.log('Ok!');
-} else {
-    console.log('Error');
-}
-
 let num = 50;
 
-(num==50) ? console.log('Ok') : console.log('Error');
-
-switch (num) {
-    case 49:
-        console.log('No');
-        break;
-    case 50:
-        console.log('Ok');
-        break;
-    default:
-        console.log('Fack');
-        break;        
+while (num < 55) {
+    console.log(num);
+    num++;
 }
+
