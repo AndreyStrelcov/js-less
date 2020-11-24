3e2f0bf1b80da44447edaddf6c59472062dc9b04
Andrew <astrelcov@yahoo.com>
2020-11-24 23:11:51 +0200

    practic2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-while (num < 55) {
-    console.log(num);
-    num++;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.lenhth < 50) {
+        personalMovieDB.movies[a] = b;  
+        console.log('done');
+    } else {
+        console.log('error');
+        --i;
+    }    
 }
 
+console.log(personalMovieDB);
