eda0f07020cb543435f99ddf3b22681fe81041a4
Andrew <astrelcov@yahoo.com>
2020-11-26 01:02:21 +0200

    object

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
 "use strict";
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+let options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    color: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("i'm method");
+    }
+};
 
-function done() {
-    console.log('Я прошел этот урок!');
-}
+const {border, bg} = options.color;
+console.log(border);
 
-learnJS('JavaScript', done);// Не указываем в функции скобки
+// delete options.name;
+
+// for (let key in options) {
+//     if (typeof(options[key] === 'object')) {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+
+options.makeTest();
+
