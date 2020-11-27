b8f029becc7f5ccc00b9efdc3fb3c6e3232e6689
Andrew <astrelcov@yahoo.com>
2020-11-28 00:49:49 +0200

    array

--- js/script.js
+++ js/script.js
@@ -1,32 +1,16 @@
 "use strict";
 
-let options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    color: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log("i'm method");
-    }
-};
+let arr = [1, 2, 3, 6, 8];
 
-const {border, bg} = options.color;
-console.log(border);
 
-// delete options.name;
-
-// for (let key in options) {
-//     if (typeof(options[key] === 'object')) {
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     }
+// for (let i=0; i < arr.length; i++) {
+//     console.log(arr[i]);
 // }
 
-options.makeTest();
+for (let value of arr) {
+    console.log(value);
+}
 
+arr.forEach(function(item, i, arr)) {
+    console.log(`${i}: ${item} intro array ${arr}`);
+}
